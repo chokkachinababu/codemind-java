bb1c8b87016dc2605abd686dba6d217b7fdd3f5b
Chinna babu chokka <84577246+chokkachinababu@users.noreply.github.com>
2023-06-06 10:37:42 +0530

    import java.util.*; public class sample{     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int p,t,r,i;         p=sc.nextInt();         t=sc.nextInt();         r=sc.nextInt();         i=(p*t*r)/100;         System.out.println(i);         sc.close();     } }

---
+++ SIMPLE_INTEREST.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class sample{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int p,t,r,i;
+        p=sc.nextInt();
+        t=sc.nextInt();
+        r=sc.nextInt();
+        i=(p*t*r)/100;
+        System.out.println(i);
+        sc.close();
+    }
+}
